d6f73b81a7325b12ebad67d38876cc9a11ea355b
qHien <tranbo158@gmail.com>
2021-06-23 06:21:30 +0700

    add Competition

--- src/demos/AboutUsPage.js
+++ src/demos/AboutUsPage.js
@@ -4,10 +4,7 @@ import tw from "twin.macro";
 import styled from "styled-components"; //eslint-disable-line
 import { css } from "styled-components/macro"; //eslint-disable-line
 import MainFeature1 from "components/features/TwoColWithButton.js";
-// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats.js";
-// import MainFeature3 from "components/features/TwoColSingleFeatureWithStats2.js";
 import Features from "components/features/ThreeColSimple.js";
-// import Features from "components/features/ThreeColWithSideImage.js";
 import WallOfText from "components/blogs/Paragraphs.js";
 import { CustomBanner as Banner } from "components/hero/BackgroundAsImageWithCenteredContent";
 import UniversitiesIconImage from "images/university.svg";
@@ -39,7 +36,7 @@ export default () => {
         imageSrc="https://cdn.pixabay.com/photo/2016/11/29/09/32/concept-1868728_960_720.jpg"
         textOnLeft={false}
       />
-      <WallOfText textContent="Enhancing urban quality and environment by excellent education, in-depth research and problem-solving projects in terms of “Think Globally - Act Locally”. Educating new generation students for creative and critical thinking, global knowledge, professional skills, sustainability perception.Developing the most advanced integrated Smart City StudioLab system with sufficient spaces and equipment for comprehensive and in-depth analysis.Impacting on urban planning, design and management by changing the way of thinking and involvement of the community, local authority and stakeholders."/>
+      <WallOfText textContent="Enhancing urban quality and environment by excellent education, in-depth research and problem-solving projects in terms of “Think Globally - Act Locally”. Educating new generation students for creative and critical thinking, global knowledge, professional skills, sustainability perception.Developing the most advanced integrated Smart City StudioLab system with sufficient spaces and equipment for comprehensive and in-depth analysis.Impacting on urban planning, design and management by changing the way of thinking and involvement of the community, local authority and stakeholders." />
       <Testimonial />
       <Features
         subheading={<Subheading>Core Values</Subheading>}
@@ -49,7 +46,8 @@ export default () => {
           {
             imageSrc: UniversitiesIconImage,
             title: "Creativity",
-            description: " For innovative education systems spurring excellence and productivity."
+            description:
+              " For innovative education systems spurring excellence and productivity.",
           },
           {
             imageSrc: CompaniesIconImage,
